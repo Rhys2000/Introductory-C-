109c1bebfebec10407cf1a6f764b01406f7fafbe
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-21 13:36:23 -0700

    Completed Hello World lesson. Learn about outputting to the console using std::cout

--- Introductory C++/main.cpp
+++ Introductory C++/main.cpp
@@ -1,14 +1,26 @@
 //
 //  main.cpp
 //  Introductory C++
-//
 //  Created by Rhys Julian-Jones on 8/21/23.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    std::cout << "Hello, World!";
+    
+    std::cout << "Codecademy\n";
+    
+    std::cout << "       1\n";
+    std::cout << "     2 3\n";
+    std::cout << "   4 5 6\n";
+    std::cout << "7 8 9 10\n";
+    
+    std::cout << "Dear Self,\n\n";
+    std::cout << "While partaking in this course I hope to learn about foundational principles of C++ and expand my understanding of programming to allow my resume to stand out against the crowd.\n\n";
+    std::cout << "Sincerely, \n";
+    std::cout << "- Rhys Julian-Jones";
+    
     return 0;
 }
