53537f1e25b13005050429ac2a3a0e5f760950c0
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-21 14:16:43 -0700

    Completed the dog years project

--- Introductory C++/main.cpp
+++ Introductory C++/main.cpp
@@ -94,5 +94,18 @@ int main() {
     kilometers = miles * 1.609344;
     std::cout << "That is " << kilometers << " in kilometers.\n";
     
+    std::cout << std::endl;
+    
+    int dog_age = 13;
+    //The first two years of a dog's life count as 21 human years.
+    int early_years = 21;
+    //Each following year counts as 4 human years.
+    int later_years = (dog_age - 2) * 4;
+    //Total number of human years the dog has lived.
+    int human_years = early_years + later_years;
+    std::cout << "My name is Beckham! Ruff ruff, I am " << human_years << " years old in human years.\n";
+    
+    std::cout << std::endl;
+    
     return 0;
 }
