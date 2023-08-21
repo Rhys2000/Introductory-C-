1659e23c21edfcdb2d055834da7d3bdcd73bbcfe
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-21 13:26:45 -0700

    Initial Commit

---
+++ Introductory C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Introductory C++
+//
+//  Created by Rhys Julian-Jones on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
