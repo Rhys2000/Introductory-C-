14b2389e01037be275c837eabde3a0b1a4b548a8
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-21 13:50:45 -0700

    Completed initials.cpp project

--- Introductory C++/main.cpp
+++ Introductory C++/main.cpp
@@ -8,14 +8,20 @@
 
 int main() {
     
-    std::cout << "Hello, World!";
+    std::cout << "Hello, World!\n";
+    
+    std::cout << std::endl;
     
     std::cout << "Codecademy\n";
+    
+    std::cout << std::endl;
     
     std::cout << "       1\n";
     std::cout << "     2 3\n";
     std::cout << "   4 5 6\n";
     std::cout << "7 8 9 10\n";
+    
+    std::cout << std::endl;
     
     std::cout << "Dear Self,\n\n";
     std::cout << "While partaking in this course I hope to learn about foundational principles of C++ and expand my understanding of programming to allow my resume to stand out against the crowd.\n\n";
@@ -23,6 +29,16 @@ int main() {
     std::cout << "- Rhys Julian-Jones";
     
     //Compile and execute C++ code using the following commands. g++ <filename>.cpp -o <name> to Compile and ./a.out or ./<name> to Execute
+    
+    std::cout << std::endl << std::endl;
+    
+    std::cout << "RRRR  JJJJJ JJJJJ\n";
+    std::cout << "R   R   J     J  \n";
+    std::cout << "R   R   J     J  \n";
+    std::cout << "RRRR    J     J  \n";
+    std::cout << "R R   J J   J J  \n";
+    std::cout << "R  R  J J   J J  \n";
+    std::cout << "R   R  JJ    JJ  \n";
     
     return 0;
 }
