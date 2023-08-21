dd56072509cd30f621062fedbe96dd49a0b20a0e
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-21 14:26:57 -0700

    Completed quadratic formula project

--- Introductory C++/main.cpp
+++ Introductory C++/main.cpp
@@ -5,6 +5,7 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 int main() {
     
@@ -106,6 +107,23 @@ int main() {
     std::cout << "My name is Beckham! Ruff ruff, I am " << human_years << " years old in human years.\n";
     
     std::cout << std::endl;
+    
+    double a, b, c;
+    std::cout << "Enter a: ";
+    std::cin >> a;
+    std::cout << "Enter b: ";
+    std::cin >> b;
+    std::cout << "Enter c: ";
+    std::cin >> c;
+    double root1, root2;
+    root1 = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+    root2 = (-b - sqrt((b * b) - (4 * a * c))) / (2 * a);
+    std::cout << "Root 1 is " << root1 << "\n";
+    std::cout << "Root 2 is " << root2 << "\n";
+    
+    std::cout << std::endl;
+    
+    
     
     return 0;
 }
